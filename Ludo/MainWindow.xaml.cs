b3e63ba7d5913ce8e288dd5e1724052f4b8f7d93
hannah.borjesson@gmail.com <hannah.borjesson@gmail.com>
2013-11-11 17:26:39 +0100

    Created a grid-structure for the boardgame. Added squares.

--- Ludo/MainWindow.xaml.cs
+++ Ludo/MainWindow.xaml.cs
@@ -22,7 +22,95 @@ namespace Ludo
     {
         public MainWindow()
         {
+
             InitializeComponent();
+           
+            // Create the Grid
+            Grid myGrid = new Grid();
+            myGrid.Width = 800;
+            myGrid.Height = 800;
+;
+           // myGrid.HorizontalAlignment = HorizontalAlignment.Left;
+            //myGrid.VerticalAlignment = VerticalAlignment.Top;
+            myGrid.ShowGridLines = true;
+
+            // Define the Columns
+            for (int i = 0; i < 5; i++) { 
+                ColumnDefinition column = new ColumnDefinition();
+                RowDefinition row = new RowDefinition();
+                myGrid.ColumnDefinitions.Add(column);
+                 myGrid.RowDefinitions.Add(row);
+            }
+           
+            // Add the Grid as the Content of the Parent Window Object
+            board.Content = myGrid;
+            board.UpdateLayout();
+            //board.myGrid = myGrid;
+             //myGrid.Show();
+
+            // Add the Ellipse to the StackPanel.
+           // myGrid.Children.Add(myEllipse);
+
+            //this.Content = myGrid;
+            // Add the left text cell to the Grid
+            myGrid.Children.Add(createGrid(1, 2, myGrid));
+            myGrid.Children.Add(createGrid(2, 1, myGrid));
+            myGrid.Children.Add(createGrid(2, 3, myGrid));
+            myGrid.Children.Add(createGrid(3, 2, myGrid));
+
+        }
+        public Grid createGrid(int rowPos, int columnPos, Grid myGrid)
+        {
+            Grid smallGrid = new Grid();
+            smallGrid.ShowGridLines = true;
+            for (int i = 0; i < 4; i++)
+            {
+                ColumnDefinition column = new ColumnDefinition();
+                RowDefinition row = new RowDefinition();
+                smallGrid.ColumnDefinitions.Add(column);
+                if (i < 3) { smallGrid.RowDefinitions.Add(row); }
+            }
+
+            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Ellipse ell = createEllipse();
+                    Grid.SetColumn(ell, j);
+                    Grid.SetRow(ell, i);
+                    smallGrid.Children.Add(ell);
+                }
+            }
+            Grid.SetRow(smallGrid, rowPos);
+            Grid.SetColumn(smallGrid, columnPos);
+            return smallGrid;
+            //myGrid.Children.Add(smallGrid);
+        }
+
+        public Ellipse createEllipse()
+        {
+            Ellipse myEllipse = new Ellipse();
+
+            // Create a SolidColorBrush with a red color to fill the  
+            // Ellipse with.
+            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+
+            // Describes the brush's color using RGB values.  
+            // Each value has a range of 0-255.
+            mySolidColorBrush.Color = Color.FromArgb(255, 255, 255, 0);
+            myEllipse.Fill = mySolidColorBrush;
+            myEllipse.StrokeThickness = 2;
+            myEllipse.Stroke = Brushes.Black;
+
+            // Set the width and height of the Ellipse.
+            myEllipse.Width = 40;
+            myEllipse.Height = 40;
+
+           
+            return myEllipse;
+        }
+           
+            // This is the end
         }
     }
-}
